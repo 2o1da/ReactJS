1727df63f541059261c2b42d955078dc3496dcd2
DaSol Kim <solda@khu.ac.kr>
2021-06-08 23:40:48 +0900

    Update the files

---
+++ learning/wds/src/App.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
+  const [resourceType, setResourceType] = useState("");
+
+  console.log("render");
+
+  useEffect(() => {
+    console.log("resource type is changed");
+  }, [resourceType]);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>
+  );
+}
